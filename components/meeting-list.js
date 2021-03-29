2007c23624b83f8ef2fbc266702ae250122d9441
Matthew Stanciu <mattbstanciu@gmail.com>
2021-03-29 09:05:39 -0400

    STRANEG COLORES

--- components/meeting-list.js
+++ components/meeting-list.js
@@ -1,12 +1,12 @@
 import { Card, Image, Box, Heading, Link, Grid } from 'theme-ui'
 const data = require('../data.json')
 
-const Meeting = ({ name, img, slug }) => (
+const Meeting = ({ name, img, slug, color1, color2 }) => (
   <Link as="a" href={`/${slug}`} sx={{ textDecoration: 'none' }} passHref>
     <Card
       variant="interactive"
       sx={{
-        backgroundImage: t => t.util.gx('red', 'orange'),
+        backgroundImage: t => t.util.gx(color1, color2),
         color: 'white',
         p: [0, 0],
         lineHeight: 0,
@@ -48,11 +48,21 @@ const MeetingList = () => {
   >
     {Object.keys(data['meetings']).map((meeting, i) => {
       const meetingData = data['meetings'][meeting]
+      const color = getColor(i)
       return (
-        <Meeting name={meetingData.title} img={meetingData.img} slug={meetingData.slug} key={i} />
+        <Meeting name={meetingData.title} img={meetingData.img} slug={meetingData.slug} color1={color[1].color1} color2={color[1].color2} key={i} />
       )})}
   </Grid>
   )
+}
+
+const getColor = (meetingIndex) => {
+  const data = require('../data.json')
+  console.log('hey?')
+  const colors = Object.entries(data['colors'])
+  console.log('colors', colors)
+  console.log('selected color', colors[meetingIndex % colors.length])
+  return colors[meetingIndex % colors.length]
 }
 
 export default MeetingList
