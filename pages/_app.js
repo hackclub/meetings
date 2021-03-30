662348dc50e77b6751ee2e02cf7971a8bff53167
Matthew Stanciu <mattbstanciu@gmail.com>
2021-03-30 12:37:46 -0400

    change social card

--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,7 @@ export default class App extends NextApp {
           name="Hack Club"
           title="Meetings"
           description="A collection of real Hack Club meetings, paired with everything you need to run them in your own club."
-          image="https://cloud-yjk47pvf1-hack-club-bot.vercel.app/0screen_shot_2021-03-28_at_2.54.20_pm.png"
+          image="https://cloud-6igzrxeem-hack-club-bot.vercel.app/0meetings.png"
         />
         <Nav />
         <Component {...pageProps} />
